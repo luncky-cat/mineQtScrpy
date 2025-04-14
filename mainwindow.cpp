211e296582f8632b8225a8ad0e8106c7f644faee
LuckyCat <zhihuah@foxmail.com>
2025-04-14 13:55:49 +0800

    主窗口界面类

---
+++ mainwindow.cpp
@@ -0,0 +1,22 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "showlayout.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+
+    showLayout *show=new showLayout();
+    QVBoxLayout * layout=new QVBoxLayout();
+    layout->addWidget(show);
+    ui->centralwidget->setLayout(layout);
+    //setLayout(layout);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
