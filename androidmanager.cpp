86d2981590dfcb4249062383993f8420c09e151b
LuckyCat <zhihuah@foxmail.com>
2025-04-14 13:53:44 +0800

    安卓设备管理类

---
+++ androidmanager.cpp
@@ -0,0 +1,39 @@
+#include "androidmanager.h"
+
+androidManager::androidManager() {}
+
+void androidManager::getDevices()
+{
+
+
+}
+
+void androidManager::closeDevices()
+{
+
+}
+
+void androidManager::allDescription()
+{
+
+}
+
+void androidManager::closeDevice()
+{
+
+}
+
+void androidManager::description()
+{
+
+}
+
+void androidManager::init()
+{
+
+}
+
+void androidManager::listen()
+{
+
+}
