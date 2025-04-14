670e5b24dc4a9fa238630164693181f3afdd1c61
LuckyCat <zhihuah@foxmail.com>
2025-04-14 13:52:46 +0800

    安卓设备类

---
+++ androiddevice.cpp
@@ -0,0 +1,18 @@
+#include "androiddevice.h"
+
+androidDevice::androidDevice() {}
+
+QString androidDevice::getDeviceInfo()
+{
+
+}
+
+QString androidDevice::getDeviceStatus()
+{
+
+}
+
+QString androidDevice::getDeviceId()
+{
+
+}
---
+++ androiddevice.h
@@ -0,0 +1,13 @@
+#ifndef ANDROIDDEVICE_H
+#define ANDROIDDEVICE_H
+
+#include "device.h"
+
+class androidDevice : public Device
+{
+    Q_OBJECT
+public:
+    androidDevice();
+};
+
+#endif // ANDROIDDEVICE_H
