f963b01baba47bef219f94caa0d783ae3b4ea7f4
LuckyCat <zhihuah@foxmail.com>
2025-04-16 23:12:04 +0800

    修改设备管理虚基类接口

--- devicemanager.h
+++ devicemanager.h
@@ -1,26 +1,18 @@
 #ifndef DEVICEMANAGER_H
 #define DEVICEMANAGER_H
+#include<QWidget>
 
-#include <QWidget>
-
-class DeviceManager : public QWidget
+class DeviceManager:public QWidget
 {
     Q_OBJECT
 public:
-    explicit DeviceManager(QWidget *parent = nullptr);
-private:
     virtual void getDevices()=0;   //获得所有设备
-    virtual void closeDevices()=0;  //关闭所有设备
     virtual void allDescription()=0;  //获得所有设备的描述
-    virtual void closeDevice()=0;   //关闭某个设备
     virtual void description()=0;   //获得某个设备描述
-    virtual void init()=0;   //初始化
-private:
-    QList<Devic>
-signals:
-    
-
-
+    virtual void closeDevices()=0;  //关闭所有设备
+    virtual void closeDevice()=0;   //关闭某个设备
+    virtual void selectDevice()=0;
+    virtual ~DeviceManager()=default;
 };
 
 #endif // DEVICEMANAGER_H
