2c3d663e4255df3fd6885294f3c6f8d04be8b0a8
luncky-cat <luncky-cat@noreply.gitcode.com>
2025-05-21 22:59:10 +0800

    调整接口

--- adbServer/interfaces/ICommandHandler.h
+++ adbServer/interfaces/ICommandHandler.h
@@ -1,6 +1,11 @@
 #ifndef ICOMMANDHANDLER_H
 #define ICOMMANDHANDLER_H
 
+#include <map>
+
+#include "context/CommandInfo.h"
+
+
 class DeviceContext;
 class ITransPort;
 
@@ -8,6 +13,40 @@ class ICommandHandler {
 public:
     virtual ~ICommandHandler() = default;
     virtual bool CommandHandler(ITransPort& transport,DeviceContext& ctx)=0;
+
+
+    static std::map<CmdType, ICommandHandler*>& handlers() {
+        static std::map<CmdType, ICommandHandler*> instance;
+        return instance;
+    }
+
+    static ICommandHandler* getHandler(CmdType type) {
+        auto it = handlers().find(type);
+        return (it != handlers().end()) ? it->second : nullptr;
+    }
+
+    static void registerHandler(CmdType type, ICommandHandler* handler) {
+        handlers()[type] = handler;
+    }
+
 };
 
 #endif // ICOMMANDHANDLER_H
+
+
+
+
+// static PushHandler& instance() {
+//     static PushHandler instance;
+//     return instance;
+// }
+
+// bool CommandHandler(ITransPort& transport, DeviceContext& ctx) override {
+//     // 处理 push 命令逻辑
+//     return true;
+// }
+
+// private:
+// PushHandler() {
+//     ICommandHandler::registerHandler(CmdType::Push, this);
+// }
