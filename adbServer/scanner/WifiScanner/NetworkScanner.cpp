ab8d3db2b6630ce2e5005a83e1c38915f82399a4
luncky-cat <luncky-cat@noreply.gitcode.com>
2025-05-25 18:10:38 +0800

    重写网络扫描器优化接口

--- adbServer/scanner/WifiScanner/NetworkScanner.cpp
+++ adbServer/scanner/WifiScanner/NetworkScanner.cpp
@@ -1,49 +1,55 @@
 #include "NetworkScanner.h"
 
-#include <iostream>
+#include "scanner/ConnectConfig/WifiConnectConfig.h"
+
+#include "utils/NetWorkUtils.h"
+
+
+NetworkScannerAutoRegister NetworkScanner::_autoRegisterScanner;
 
 void NetworkScanner::start()
 {
-
+    startPeriodicScan();
 }
 
 void NetworkScanner::stop()
 {
-
+    stopPeriodicScan();
 }
 
 void NetworkScanner::configure(const std::shared_ptr<IConfig> &config)
 {
-
+    auto specificConfig = std::dynamic_pointer_cast<WifiConnectConfig>(config);
+    if(specificConfig){
+        config_=specificConfig;
+    }
 }
 
-NetworkScanner::NetworkScanner(uint16_t port, int timeoutMs)
-    : port(port), timeout_ms(timeoutMs) {}
+
+void NetworkScanner::setContext(std::shared_ptr<asio::io_context>& context)
+{
+    io_context=context;
+}
 
 void NetworkScanner::setDeviceFoundCallback(DeviceFoundCallback cb) {
     device_callback = std::move(cb);
 }
 
-void NetworkScanner::scanRange(int startSuffix, int endSuffix) {
-    for (int i = startSuffix; i <= endSuffix; ++i) {
-        std::string ip = base_ip + std::to_string(i);
-        startScan(ip);
-    }
-}
 
-void NetworkScanner::startScan(const std::string& ip) {
-    if (scanned_ips.count(ip)) return;
-    scanned_ips.insert(ip);
+void NetworkScanner::startScan(const std::string& ip,uint16_t port,int timeout_ms) {    //扫描单个ip
+    auto target = std::make_pair(ip, port);
+    if (scanned_targets.count(target)) return;
+    scanned_targets.insert(target);
 
     auto socket = std::make_shared<asio::ip::tcp::socket>(io_context);
-    asio::ip::tcp::endpoint endpoint(asio::ip::make_address(ip), port);
+    asio::ip::tcp::endpoint endpoint(asio::ip::make_address(ip),port);
 
     auto timer = std::make_shared<asio::steady_timer>(io_context);
     timer->expires_after(std::chrono::milliseconds(timeout_ms));
 
-    socket->async_connect(endpoint, [this, ip, socket, timer](const asio::error_code& ec) {
+    socket->async_connect(endpoint, [this, ip,port,socket, timer](const asio::error_code& ec) {
         timer->cancel();
-        handleConnect(ip, socket, ec);
+        handleConnect(ip, port,socket, ec);
     });
 
     timer->async_wait([socket](const asio::error_code& ec) {
@@ -54,39 +60,84 @@ void NetworkScanner::startScan(const std::string& ip) {
     });
 }
 
-void NetworkScanner::handleConnect(const std::string& ip, std::shared_ptr<asio::ip::tcp::socket> socket, const asio::error_code& ec) {
-    if (!ec) {
-        if (device_callback) {
-            device_callback(ip);
+uint32_t NetworkScanner::ipToInt(const std::string& ip) {
+    std::stringstream ss(ip);
+    uint32_t a, b, c, d;
+    char dot;
+    ss >> a >> dot >> b >> dot >> c >> dot >> d;
+    return (a << 24) | (b << 16) | (c << 8) | d;
+}
+
+std::string NetworkScanner::intToIp(uint32_t ip) {
+    return std::to_string((ip >> 24) & 0xFF) + "." +
+           std::to_string((ip >> 16) & 0xFF) + "." +
+           std::to_string((ip >> 8) & 0xFF) + "." +
+           std::to_string(ip & 0xFF);
+}
+
+// 自动模式：获取当前网络环境并推导扫描范围
+bool NetworkScanner::updateNetworkInfo(){
+    if(!NetWorkUtils::getActiveWifiInfo(ip,netmask)){
+        return false;
+    }
+
+    if(!NetWorkUtils::getIpRange(ip,netmask,startIp,endIp)){
+        return false;
+    }
+
+    return true;
+}
+
+
+void NetworkScanner::scanFromIpRangeAny(const std::string& startIp, const std::string& endIp,uint16_t startPort,uint16_t endPort, int timeoutMs) {
+    uint32_t start = ipToInt(startIp);
+    uint32_t end = ipToInt(endIp);
+    if (start > end) std::swap(start, end);
+    if (startPort > endPort) std::swap(startPort, endPort);
+
+    for (uint32_t ip = start; ip <= end; ++ip) {
+        std::string ipStr = intToIp(ip);  // 避免重复转换
+        for (uint16_t port = startPort; port <= endPort; ++port) {
+            startScan(ipStr, port, timeoutMs);
         }
     }
 }
 
-void NetworkScanner::startPeriodicScan(int intervalMs, int startSuffix, int endSuffix) {
-    scan_interval_ms = intervalMs;
-    scan_range_start = startSuffix;
-    scan_range_end = endSuffix;
-    periodic_enabled = true;
+
+void NetworkScanner::handleConnect(const std::string& ip,uint16_t port,std::shared_ptr<asio::ip::tcp::socket> socket, const asio::error_code& ec) {
+    if (!ec) {
+        if (device_callback) {
+           device_callback(ip, port, socket);
+        }
+    }
+}
+
+
+
+void NetworkScanner::startPeriodicScan() {
+    if (!updateNetworkInfo()) return;
 
     if (!periodic_timer) {
-        periodic_timer = std::make_unique<asio::steady_timer>(io_context);
+        periodic_timer = std::make_unique<asio::steady_timer>(*io_context);
     }
 
-    periodic_timer->expires_after(std::chrono::milliseconds(scan_interval_ms));
+    periodic_timer->expires_after(std::chrono::milliseconds(config_->intervalMms));
+
     periodic_timer->async_wait([this](const asio::error_code& ec) {
-        if (!ec && periodic_enabled) {
-            scanned_ips.clear();
-            scanRange(scan_range_start, scan_range_end);
-            startPeriodicScan(scan_interval_ms, scan_range_start, scan_range_end);
+        if (!ec &&config_->periodicEnabled) {
+            // scanned_targets.clear();
+            scanFromIpRangeAny(startIp, endIp, config_->startPort, config_->endPort, config_->timeoutMs);
+            startPeriodicScan(); // 递归再次启动定时器
         }
     });
 }
 
 void NetworkScanner::stopPeriodicScan() {
-    periodic_enabled = false;
+    config_->periodicEnabled = false;
     if (periodic_timer) {
         asio::error_code ec;
         periodic_timer->cancel(ec);
     }
 }
+
 
--- adbServer/scanner/WifiScanner/NetworkScanner.h
+++ adbServer/scanner/WifiScanner/NetworkScanner.h
@@ -4,46 +4,63 @@
 #include <asio/steady_timer.hpp>
 #include <asio/io_context.hpp>
 #include<asio/ip/tcp.hpp>
-#include <unordered_set>
+#include <set>
 
 #include"interfaces/IScanner.h"
 
 
+struct NetworkScannerAutoRegister;
+class WifiConnectConfig;
+
 class NetworkScanner :public IScanner{
 public:
-
     void start() override;
 
     void stop() override;
 
     void configure(const std::shared_ptr<IConfig>& config) override;
 
+    void setContext(std::shared_ptr<asio::io_context>& context);
 
-    using DeviceFoundCallback = std::function<void(const std::string& ip)>;
-    NetworkScanner(uint16_t port=5555, int timeoutMs = 300);
-
-    void scanRange(int startSuffix, int endSuffix);
-    void setDeviceFoundCallback(DeviceFoundCallback cb);
-    void startPeriodicScan(int intervalMs, int startSuffix, int endSuffix);
-    void stopPeriodicScan();
 
 private:
-    void startScan(const std::string& ip);
-    void handleConnect(const std::string& ip, std::shared_ptr<asio::ip::tcp::socket> socket, const asio::error_code& ec);
+    NetworkScanner()=default;
+    NetworkScanner(const NetworkScanner&)=delete;
+    NetworkScanner& operator=(const NetworkScanner&) = delete;
 
-    std::shared_ptr<asio::io_context> io_context;
-    std::string base_ip;
-    uint16_t port;
-    int timeout_ms;
+private:
+    using DeviceFoundCallback = std::function<void(const std::string& ip, uint16_t port, std::shared_ptr<asio::ip::tcp::socket> socket)>;
     DeviceFoundCallback device_callback;
+    void setDeviceFoundCallback(DeviceFoundCallback cb);
 
-    std::unordered_set<std::string> scanned_ips;
-
+    std::string intToIp(uint32_t ip);
+    uint32_t ipToInt(const std::string &ip);
+    void startScan(const std::string &ip, uint16_t port, int timeout_ms);
+    void scanFromIpRangeAny(const std::string &startIp, const std::string &endIp, uint16_t startPort, uint16_t endPort, int timeoutMs);
+    bool updateNetworkInfo();   //更新网络信息
+    void startPeriodicScan();  //开启定时任务
+    void stopPeriodicScan();
+    void handleConnect(const std::string &ip, uint16_t port, std::shared_ptr<asio::ip::tcp::socket> socket, const asio::error_code &ec);
+private:
+    std::shared_ptr<asio::io_context> io_context;
+    std::shared_ptr<WifiConnectConfig> config_;
+    std::set<std::pair<std::string, uint16_t>> scanned_targets;
     std::unique_ptr<asio::steady_timer> periodic_timer;
-    int scan_interval_ms = 0;
-    int scan_range_start = 0;
-    int scan_range_end = 0;
-    bool periodic_enabled = false;
+    static NetworkScannerAutoRegister _autoRegisterScanner;
+
+    std::string ip;
+    std::string netmask;
+    std::string startIp;
+    std::string endIp;
+
+
+};
+
+struct NetworkScannerAutoRegister {
+    NetworkScannerAutoRegister() {
+        auto scanner = std::make_shared<NetworkScanner>();
+        IScanner::registerScanner(scanner);
+    }
 };
 
 #endif // NETWORKSCANNER_H
