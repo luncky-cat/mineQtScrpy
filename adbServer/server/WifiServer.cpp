ec6c48b8ebc26484fc6c6ea6cd7f8757098651f3
luncky-cat <luncky-cat@noreply.gitcode.com>
2025-05-29 17:49:26 +0800

    更新接口，重构

--- adbServer/server/WifiServer.cpp
+++ adbServer/server/WifiServer.cpp
@@ -20,26 +20,13 @@ std::shared_ptr<WifiServer> WifiServer::instance() {
     return inst;
 }
 
-bool WifiServer::connect(DeviceContext &ctx)
+WifiServer::WifiServer()
 {
+    qDebug()<<"wifiservr构造";
+}
 
-    auto &info = ctx.connectInfo;
-    std::string ip = info.ipAddress.toStdString();
-    unsigned short port = static_cast<unsigned short>(info.port);
-
-    // asio::ip::tcp::socket socket(io_context);  // 使用成员 io_context
-
-    // try {
-    //     asio::ip::tcp::endpoint endpoint(asio::ip::make_address(ip), port);
-    //     socket.connect(endpoint);
-    // }
-    // catch (std::system_error& e) {
-    //     printf("Connect failed: %s\n", e.what());
-    //     return false;
-    // }
-
-    // ctx.setSocket(std::move(socket));  // 传入 asio socket，移动语义
-
+bool WifiServer::connect(std::shared_ptr<DeviceContext>ctx)
+{
     return true;
 }
 
@@ -66,70 +53,79 @@ std::map<std::string, std::string> parseDeviceInfo(const std::vector<uint8_t>& p
     return infoMap;
 }
 
+// 连接请求
+void WifiServer::startConnect(std::shared_ptr<DeviceContext>Ctx) {
+    Ctx->sessionCtx->send(AdbProtocol::generateConnect());
+    Ctx->sessionCtx->waitConnectCommand({AdbProtocol::CMD_AUTH},100,
+                                        [this,Ctx](const AdbMessage& tokenMsg) {
+                                            handleAuthToken(Ctx,tokenMsg);
+                                        });
+}
 
-bool WifiServer::auth(DeviceContext& ctx) {
-    AdbMessage Msg;
-    std::vector<uint8_t> connectMessage = AdbProtocol::generateConnect();
-    ctx.sessionCtx->send(connectMessage);
+// 第二步：收到 AUTH TOKEN，签名并发送
+void WifiServer::handleAuthToken(std::shared_ptr<DeviceContext>Ctx, const AdbMessage& tokenMsg) {
+    qDebug() << "[handleAuthToken] payload size = " << tokenMsg.payload.size();
+    auto sigload = CryptoUtils::getInstance().signAdbTokenPayload(tokenMsg.payload);
+    qDebug() << "[handleAuthToken] 签名长度: " << sigload.size();
+    auto sigMsg = AdbProtocol::generateAuth(AdbProtocol::AUTH_TYPE_SIGNATURE, sigload);
+    qDebug() << "[handleAuthToken] 准备发送签名";
+    Ctx->sessionCtx->send(sigMsg);
+    qDebug() << "[handleAuthToken] 发送完毕";
+    Ctx->sessionCtx->waitConnectCommand({AdbProtocol::CMD_AUTH, AdbProtocol::CMD_CNXN},100,
+                                        [this, Ctx](const AdbMessage& msg) {
+                                            if (msg.command == AdbProtocol::CMD_AUTH) {
+                                                qDebug() << "拒绝密钥";
+                                                handleAuthRejected(Ctx);
+                                            } else {
+                                                qDebug() << "接受密钥";
+                                                handleAuthAccepted(Ctx, msg);
+                                            }
+                                        });
+}
 
-    // 等待 AUTH 请求（type = TOKEN）
-    if (!ctx.sessionCtx->tryFindNextCommand({AdbProtocol::CMD_AUTH}, Msg)) {
-        qDebug() << "未收到 AUTH TOKEN";
-        return false;
-    }
+// 第三步：签名失败，发送公钥
+void WifiServer::handleAuthRejected(std::shared_ptr<DeviceContext>Ctx) {
+    auto pubKey = CryptoUtils::getInstance().getPublicKeyBytes();
+    auto pubKeyMsg = AdbProtocol::generateAuth(AdbProtocol::AUTH_TYPE_RSA_PUBLIC, pubKey);
+    Ctx->sessionCtx->send(pubKeyMsg);
+    qDebug()<<"发送公钥";
+    Ctx->sessionCtx->waitConnectCommand({AdbProtocol::CMD_CNXN},100,
+                                        [this,Ctx](const AdbMessage& cnxnMsg) {
+                                            qDebug()<<"连接成功";
+                                            handleAuthAccepted(Ctx, cnxnMsg);
+                                        });
+}
 
-    // 签名 TOKEN 并发送 AUTH(type=signature)
-    std::vector<uint8_t> sigload = CryptoUtils::getInstance().signAdbTokenPayload(Msg.payload);
-    std::vector<uint8_t> sigMsg = AdbProtocol::generateAuth(AdbProtocol::AUTH_TYPE_SIGNATURE, sigload);
-    ctx.sessionCtx->send(sigMsg);
-
-    // 再等待回应，可能是 CNXN 或再次 AUTH（type=public key）
-    // 这里用两个命令重载，支持两个命令判断
-    if (!ctx.sessionCtx->tryFindNextCommand({AdbProtocol::CMD_AUTH, AdbProtocol::CMD_CNXN}, Msg)) {
-        qDebug() << "签名后无回应";
-        return false;
-    }
-
-    if (Msg.command == AdbProtocol::CMD_AUTH) {
-        // 签名不被信任，发送 PUBLIC KEY
-        std::vector<uint8_t> pubKey = CryptoUtils::getInstance().getPublicKeyBytes();
-        std::vector<uint8_t> authPubKeyMsg = AdbProtocol::generateAuth(AdbProtocol::AUTH_TYPE_RSA_PUBLIC, pubKey);
-        ctx.sessionCtx->send(authPubKeyMsg);
-        // 最后等 CNXN
-        if (!ctx.sessionCtx->tryFindNextCommand({AdbProtocol::CMD_CNXN}, Msg)) {
-            qDebug() << "发送公钥后未连接成功";
-            return false;
-        }
-    } else if (Msg.command != AdbProtocol::CMD_CNXN) {
-        qDebug() << "签名后收到意外命令";
-        return false;
-    }
-
-    // 解析设备信息
-    ctx.authInfos = parseDeviceInfo(Msg.payload);
+// 第四步：认证成功，解析设备信息
+void WifiServer::handleAuthAccepted(std::shared_ptr<DeviceContext>Ctx, const AdbMessage& cnxnMsg) {
+    Ctx->authInfos=parseDeviceInfo(cnxnMsg.payload);
     qDebug() << "设备信息:";
-    for (const auto& i : ctx.authInfos) {
-        qDebug() << i.first << " = " << i.second;
+    for (const auto& kv : Ctx->authInfos){
+        qDebug() << kv.first << "=" << kv.second;
     }
+}
 
+
+bool WifiServer::auth(std::shared_ptr<DeviceContext>ctx) {
+    startConnect(ctx);
     return true;
 }
 
 
-bool WifiServer::execute(DeviceContext &ctx)    //后续解耦
+bool WifiServer::execute(std::shared_ptr<DeviceContext>ctx)    //后续解耦
 {
     testPushThenList(ctx);
     return true;
 }
 
-void WifiServer::testPushThenList(DeviceContext &ctx)
+void WifiServer::testPushThenList(std::shared_ptr<DeviceContext>ctx)
 {
     // // ===============================
     // // Step 1. Push 文件到设备
     // // ===============================
 
-    // //"D:\\Documents\\mineQtScrcpy\\CMakeLists.txt",
-    // //"/data/local/tmp/CMakeLists.txt"
+    //"D:\\Documents\\mineQtScrcpy\\CMakeLists.txt",
+    //"/data/local/tmp/CMakeLists.txt"
     // ctx.cmd.params.clear();
     // ctx.cmd.type = CmdType::Push;
 
@@ -137,23 +133,23 @@ void WifiServer::testPushThenList(DeviceContext &ctx)
     //     "D:/Documents/mineQtScrcpy/scrcpy/scrcpy-server",
     //     "/data/local/tmp/scrcpy-server.jar"
     // };
-    // // ctx.cmd.params = {
-    // //     "D:\\Documents\\mineQtScrcpy\\CMakeLists.txt",
-    // //     "/data/local/tmp/CMakeLists.jar"
-    // // };
+    ctx->cmd.params = {
+        "D:\\Documents\\mineQtScrcpy\\CMakeLists.txt",
+        "/data/local/tmp/CMakeLists.jar"
+    };
 
-    // qDebug() << "开始推送文件到设备";
-    // if (auto handler = ICommandHandler::getHandler(ctx.cmd.type)) {
-    //     if (handler->CommandHandler(*ctx.transPort,ctx)) {
-    //         qDebug() << "文件推送成功";
-    //     } else {
-    //         qDebug() << "文件推送失败";
-    //         return;
-    //     }
-    // } else {
-    //     qDebug() << "未找到 Push 命令处理器";
-    //     return;
-    // }
+    qDebug() << "开始推送文件到设备";
+    if (auto handler = ICommandHandler::getHandler(ctx.cmd.type)) {
+        if (handler->CommandHandler(*ctx.transPort,ctx)) {
+            qDebug() << "文件推送成功";
+        } else {
+            qDebug() << "文件推送失败";
+            return;
+        }
+    } else {
+        qDebug() << "未找到 Push 命令处理器";
+        return;
+    }
 
     // // ===============================
     // // Step 2. 执行 shell 查看目标目录
@@ -180,7 +176,7 @@ void WifiServer::testPushThenList(DeviceContext &ctx)
 
 
 
-bool WifiServer::close(DeviceContext &ctx)
+bool WifiServer::close(std::shared_ptr<DeviceContext>ctx)
 {
     // if (networkInitialized) {
     //     WSACleanup();
--- adbServer/server/WifiServer.h
+++ adbServer/server/WifiServer.h
@@ -3,20 +3,26 @@
 
 #include <memory>
 
+#include "context/DeviceSession.h"
 #include"interfaces/IServer.h"
 
 class WifiServer :public IServer,public std::enable_shared_from_this<WifiServer>  //通用的wifi服务接口
 {
 public:
-    bool connect(DeviceContext& ctx) override;
-    bool auth(DeviceContext& ctx) override;
-    bool execute(DeviceContext& ctx)override;
-    bool close(DeviceContext& ctx)override;
+    bool connect(std::shared_ptr<DeviceContext>ctx) override;
+    bool auth(std::shared_ptr<DeviceContext>ctx) override;
+    bool execute(std::shared_ptr<DeviceContext>ctx)override;
+    bool close(std::shared_ptr<DeviceContext>ctx)override;
     static std::shared_ptr<WifiServer> instance();
     ~WifiServer()=default;
-    WifiServer()=default;
+    WifiServer();
 private:
-    void testPushThenList(DeviceContext &ctx);
+     void handleAuthToken(std::shared_ptr<DeviceContext> Ctx, const AdbMessage &tokenMsg);
+    void handleAuthRejected(std::shared_ptr<DeviceContext> ctx);
+    void handleAuthAccepted(std::shared_ptr<DeviceContext> ctx, const AdbMessage &cnxnMsg);
+    void startConnect(std::shared_ptr<DeviceContext> ctx);
+private:
+    void testPushThenList(std::shared_ptr<DeviceContext>ctx);
 };
 
 #endif // WIFISERVER_H
