67b5ab9cd2a04cc96eaf7705aedc2fa34ced3483
luncky-cat <luncky-cat@noreply.gitcode.com>
2025-06-04 14:01:34 +0800

    添加调试信息，增加异步相关接口，优化相关逻辑

--- adbServer/context/DeviceSession.cpp
+++ adbServer/context/DeviceSession.cpp
@@ -1,26 +1,46 @@
 #include "DeviceSession.h"
 
-#include<asio/write.hpp>
-
 #include <QEventLoop>
 #include <QTimer>
 #include <qDebug>
 
-#include "protocol/AdbProtocol.h"
+#include <asio/awaitable.hpp>
+#include<asio/write.hpp>
 
+#include "asio/use_awaitable.hpp"
+#include "context/wait_command_async.h"
+#include "protocol/AdbProtocol.h"
+#include"AdbStreamContext.h"
+#include "WaitCommandAwaitable.h"
 
 DeviceSession::DeviceSession(std::shared_ptr<asio::ip::tcp::socket> socket)
     : socket_(std::move(socket)) {}
+
+
+DeviceSession::~DeviceSession(){}
+
 
 void DeviceSession::start() {
     qDebug()<<"开始会话";
     doRead();
 }
 
+
+void DeviceSession::send(const std::vector<uint8_t>& data) {
+    std::lock_guard<std::mutex> lock(sendMutex_);
+    bool writing = !sendQueue_.empty(); // 如果队列为空，说明没有正在发送
+    sendQueue_.push_back(data);
+    if (!writing) {
+        doWrite(); // 启动异步写
+    }
+}
+
+
 void DeviceSession::doRead() {
     auto self(shared_from_this()); // 保证异步回调期间对象不会被释放
     socket_->async_read_some(asio::buffer(tempBuf_),
                              [this, self](std::error_code ec, std::size_t length) {
+                                 qDebug() << "[doRead]async_read_some 回调触发了，长度：" << length << " 错误：" << ec.message();
                                  if (!ec) {
                                      // 将读取数据追加到缓冲区
                                      recvBuffer_.insert(recvBuffer_.end(), tempBuf_.data(), tempBuf_.data() + length);
@@ -41,41 +61,23 @@ void DeviceSession::doRead() {
                              });
 }
 
+
 void DeviceSession::handleMessage(const AdbMessage& msg) {    //分发
-    std::lock_guard<std::mutex> lock(waiterMutex_);
-    qDebug()<<"接受完整数据开始处理分发";
+    qDebug()<<"[handleMessage]接受完整数据开始处理分发";
     dispatchToContext(msg); // 将消息交由上层处理（按 stream/ctx 分发）
-    //唤醒等待
-    //checkPendingWaiters();
-
 }
-
-
-bool DeviceSession::tryGetStreamMessage(uint32_t streamId, AdbMessage& outMsg) {  //遍历取专属流包
-    auto it = streamContexts.find(streamId);
-    if (it == streamContexts.end()) {
-        return false;
-    }
-    auto msg = it->second->tryDequeueMessage();
-    if (!msg.has_value()){
-        return false;
-    }
-    outMsg = std::move(msg.value());
-    return true;
-}
-
 
 
 QString DeviceSession::adbCommandToString(uint32_t cmd) {
-    char buf[5] = {
-        static_cast<char>(cmd & 0xFF),
-        static_cast<char>((cmd >> 8) & 0xFF),
-        static_cast<char>((cmd >> 16) & 0xFF),
-        static_cast<char>((cmd >> 24) & 0xFF),
-        '\0'
-    };
-    return QString::fromLatin1(buf);
+    char chars[5];  // 4字符 + \0
+    chars[0] = static_cast<char>((cmd) & 0xFF);
+    chars[1] = static_cast<char>((cmd >> 8) & 0xFF);
+    chars[2] = static_cast<char>((cmd >> 16) & 0xFF);
+    chars[3] = static_cast<char>((cmd >> 24) & 0xFF);
+    chars[4] = '\0';
+    return QString::fromLatin1(chars);
 }
+
 bool DeviceSession::matchCommand(const std::vector<uint32_t>& cmdsexpectedCmds, AdbMessage& inputMsg) {
     qDebug() << "[matchCommand] 当前消息命令:" << inputMsg.command
              << " (" << adbCommandToString(inputMsg.command) << ")";
@@ -91,119 +93,122 @@ bool DeviceSession::matchCommand(const std::vector<uint32_t>& cmdsexpectedCmds, 
     return false;
 }
 
+std::deque<AdbMessage>& DeviceSession::findMessageDeque(uint32_t streamId){   //找寻消息队列
+    qDebug()<<"[findMessageDeque]找寻消息队列";
+    auto it = streamContexts.find(streamId);
+    if (it == streamContexts.end()) {
+         qDebug()<<"[findMessageDeque]默认消息队列";
+        return pendingMessages;
+    }
+     qDebug()<<"[findMessageDeque]特定消息队列";
+    return it->second->getStreamDeque();
+}
 
-bool DeviceSession::tryFindSessionCommand(const std::vector<uint32_t>& expectedCmds,AdbMessage &outMsg) { //遍历取无归属流包
-    qDebug() << "查找匹配命令包";
-    for (auto it = pendingMessages.begin(); it != pendingMessages.end(); ++it) {
-        if(matchCommand(expectedCmds,*it)){
+std::list<commandWaiter>& DeviceSession::findWaiters(uint32_t streamId){   //找寻等待者
+    auto it = streamWaiters.find(streamId);
+    if (it == streamWaiters.end()) {
+        throw std::runtime_error("Stream ID not found");
+    }
+    return it->second;
+}
+
+bool DeviceSession::findCommandByDeque(const std::vector<uint32_t>& expectedCmds,std::deque<AdbMessage>&messages,AdbMessage &outMsg) { //队列中找包
+    qDebug() << "[findCommandByDeque]查找匹配命令包";
+    for (auto it = messages.begin(); it != messages.end();) {
+        if(matchCommand(expectedCmds, *it)) {
             outMsg = std::move(*it);
-            pendingMessages.erase(it); // 原地删除，避免额外拷贝
-            qDebug()<<"pendingMessages的大小"<<pendingMessages.size();
-            qDebug() << "找到匹配的命令包";
+            it = messages.erase(it);  // erase返回下一个有效迭代器
+            qDebug() << "[findCommandByDeque]找到匹配的命令包";
             return true;
-        }
-    }
-
-    qDebug() << "未找到匹配的命令包";
-    return false;
-}
-
-bool DeviceSession::waitConnectCommand(const std::vector<uint32_t>& expectedCmds,int timeoutMs,std::function<void(const AdbMessage&)> cb){   //等待连接命令
-    AdbMessage msg;
-    if (tryFindSessionCommand(expectedCmds,msg)) {
-        cb(msg);
-        qDebug()<<"开始就找到了包，等待者"<<sessionWaits.size();
-        return true;
-    }
-    sessionWaits.push_back({expectedCmds,std::chrono::steady_clock::now() + std::chrono::seconds(timeoutMs),cb});
-    qDebug()<<"当前等待"<<sessionWaits.size();
-    return false;
-}
-
-
-void DeviceSession::checkSessionWaiters() {
-    qDebug()<<"唤醒";
-    for (auto it = sessionWaits.begin(); it != sessionWaits.end(); ) {
-        if (it->expireTime < std::chrono::steady_clock::now()) {    //超时移除
-            it = sessionWaits.erase(it);
-            continue;
-        }
-
-        AdbMessage msg;
-        if (tryFindSessionCommand(it->expectedCmds, msg)) {
-            auto cb = std::move(it->callback);  // callback 是 std::function<void(AdbMessage)>
-            it = sessionWaits.erase(it);        // 先删除，避免迭代器失效
-            cb(msg);
         } else {
             ++it;
         }
     }
+    qDebug() << "[findCommandByDeque]未找到匹配的命令包";
+    return false;
+}
+
+asio::any_io_executor DeviceSession::getExecutor() const {
+    return socket_->get_executor();
+}
+
+asio::awaitable<AdbMessage> DeviceSession::coWaitCommand(
+    std::vector<uint32_t> expectedCmds, int timeoutMs, uint32_t streamId
+    ) {
+    co_return co_await async_wait_command(shared_from_this(), std::move(expectedCmds), timeoutMs, streamId, asio::use_awaitable);
 }
 
 
+void DeviceSession::addWaiter(uint32_t streamId,commandWaiter& waiter){
+    qDebug()<<"[addWaiter]添加等待者:流id"<<streamId;
+    streamWaiters[streamId].emplace_back(std::move(waiter));
+}
 
+std::shared_ptr<AdbStreamContext> DeviceSession::createStreamContext(uint32_t localId) {   //创建流的接口
+    qDebug()<<"创建流对象:"<<localId;
+    auto ctx = std::make_shared<AdbStreamContext>(localId);
+    streamContexts[localId] = std::move(ctx);
+    return ctx;
+}
 
+void DeviceSession::removeStreamContext(uint32_t localId){   //销毁流
+    auto it=streamContexts.find(localId);
+    if(it!=streamContexts.end()){
+        streamContexts.erase(it);
+    }
+}
 
-
-
-
-
-
-// bool DeviceSession::waitStreamCommand(const std::vector<uint32_t>& expectedCmds, uint32_t streamId,int timeoutMs,std::function<void(const AdbMessage&)> cb){   //等待连接命令
-//     AdbMessage msg;
-//     if (tryGetStreamMessage(streamId,msg) && matchCommand(expectedCmds, msg)) {
-//         cb(msg);
-//         return true;
-//     }
-//    // streamWaits[streamId].emplace_back({expectedCmds,streamId,timeoutMs,cb});
-//     return false;
-// }
-
-
-
-DeviceSession::~DeviceSession()
-{
-
+void DeviceSession::bindRemoteStreamId(uint32_t localId, uint32_t remoteId) {
+    auto it = streamContexts.find(localId);
+    if (it != streamContexts.end()) {
+        it->second->setRemoteId(remoteId);
+    } else {
+        qWarning() << "绑定远程 ID 失败，localId 未找到:" << localId;
+    }
 }
 
 
+void DeviceSession::dispatchToContext(const AdbMessage& msg) {
+    qDebug()<<"[dispatchToContext]分发前打印msg的命令"<<adbCommandToString(msg.command);
 
-void DeviceSession::dispatchToContext(const AdbMessage& msg) {    //分发
-    qDebug()<<"分发前打印msg的命令"<<msg.command;
-    if (msg.arg1 == 0) {
-        qDebug()<<"插入无归属流队列";
+    if (msg.command == AdbProtocol::CMD_CNXN) {
+        qDebug()<<"[dispatchToContext]CNXN消息，插入无归属流队列";
         pendingMessages.emplace_back(msg);
-        qDebug()<<"pendingMessages size"<<pendingMessages.size();
-        checkSessionWaiters();
-    }else{
-        // 流消息
-        qDebug()<<"插入归属流队列";
+        qDebug()<<"[dispatchToContext]pendingMessages size"<<pendingMessages.size();
+        checkWaiters(streamWaiters[0],pendingMessages);  //默认流等待者
+    } else if (msg.arg1 == 0) {
+        qDebug()<<"[dispatchToContext]默认流消息，插入无归属流队列";
+        pendingMessages.emplace_back(msg);
+        qDebug()<<"[dispatchToContext]pendingMessages size"<<pendingMessages.size();
+        checkWaiters(streamWaiters[0],pendingMessages);
+    } else {
+        qDebug()<<"[dispatchToContext]插入归属流队列";
         auto it = streamContexts.find(msg.arg1);
+        qDebug()<<"[dispatchToContext]流队列配对:"<<"local_id"<<msg.arg1<<msg.arg0;
         if (it != streamContexts.end()) {
+            qDebug()<<"[dispatchToContext]插入成功";
             it->second->enqueueMessage(msg);
+            auto & waiters=findWaiters(msg.arg1);
+            auto &deque=findMessageDeque(msg.arg1);
+            checkWaiters(waiters,deque);
         } else {
-            // 可选：记录一个警告日志，表示非法/未知 stream 消息
-            pendingMessages.emplace_back(msg);  // fallback 处理
+            qDebug()<<"[dispatchToContext]未知流，插入无归属";
+            pendingMessages.emplace_back(msg);
         }
     }
-    //checkPendingWaiters();
 }
 
 
 
+
+void DeviceSession::markServiceOpened(const std::string& name) {
+    openedServices.insert(name);
+}
 
 bool DeviceSession::isServiceOpened(const std::string& name) const {
     return openedServices.find(name) != openedServices.end();
 }
 
-void DeviceSession::send(const std::vector<uint8_t>& data) {
-    std::lock_guard<std::mutex> lock(sendMutex_);
-    bool writing = !sendQueue_.empty(); // 如果队列为空，说明没有正在发送
-    sendQueue_.push_back(data);
-    if (!writing) {
-        doWrite(); // 启动异步写
-    }
-}
 
 void DeviceSession::doWrite() {
     auto self(shared_from_this()); // 保持会话对象在异步过程中有效
--- adbServer/context/DeviceSession.h
+++ adbServer/context/DeviceSession.h
@@ -7,70 +7,102 @@
 #include <memory>
 #include <mutex>
 #include <set>
+#include<qdebug.h>
 
 #include<asio/ip/tcp.hpp>
 
-#include "AdbStreamContext.h"
+#include <asio/awaitable.hpp>
+
 #include "ForwardBridge.h"
+#include "protocol/AdbMessage.h"
 
+class AdbStreamContext;
 struct AdbMessage;
-
-struct sessionWaiter{
-    std::vector<uint32_t> expectedCmds;
-    std::chrono::steady_clock::time_point expireTime;
-    std::function<void(const AdbMessage&)> callback;
-};
+class WaitCommandAwaitable;
 
 struct commandWaiter {
     std::vector<uint32_t> expectedCmds;
-    uint32_t streamId;
     std::chrono::steady_clock::time_point expireTime;
-    std::function<void(const AdbMessage&)> callback;
+    std::function<void(const AdbMessage&)> notify;
 };
 
 class DeviceSession :public std::enable_shared_from_this<DeviceSession> {
 public:
     DeviceSession(std::shared_ptr<asio::ip::tcp::socket> socket);
+    ~DeviceSession();
+public:
+    //工具，启动
     void start(); // 启动读循环
     void send(const std::vector<uint8_t>& data); // 发送数据
-    bool isServiceOpened(const std::string &name) const;
-    ~DeviceSession();
-
-    void addMessageListener(std::shared_ptr<std::function<void(const AdbMessage&)>> listener);
-    void removeMessageListener(std::shared_ptr<std::function<void(const AdbMessage&)>> listener);
     QString adbCommandToString(uint32_t cmd);
-    bool waitConnectCommand(const std::vector<uint32_t> &expectedCmds, int timeoutMs, std::function<void (const AdbMessage &)> cb);
-    bool waitStreamCommand(const std::vector<uint32_t> &expectedCmds, uint32_t streamId, int timeoutMs, std::function<void (const AdbMessage &)> cb);
-    bool tryFindSessionCommand(const std::vector<uint32_t> &expectedCmds, AdbMessage &outMsg);
+    bool matchCommand(const std::vector<uint32_t> &cmdsexpectedCmds, AdbMessage &inputMsg);
+
+    //服务
+    bool isServiceOpened(const std::string &name) const;
+    void markServiceOpened(const std::string &name);
+
+    //流管理
+    std::shared_ptr<AdbStreamContext> createStreamContext(uint32_t localId);
+    void removeStreamContext(uint32_t localId);
+    void bindRemoteStreamId(uint32_t localId, uint32_t remoteId);
+
+    bool findCommandByDeque(const std::vector<uint32_t> &expectedCmds, std::deque<AdbMessage> &messages, AdbMessage &outMsg); //队列里找命令
+    std::deque<AdbMessage> &findMessageDeque(uint32_t streamId=0);   //默认无归属
+    void addWaiter(uint32_t streamId, commandWaiter& waiter);
+
 private:
     void doRead(); // 异步读取
     void doWrite(); // 异步写入
     void handleMessage(const AdbMessage& msg); // 处理解析后的 ADB 消息
     void dispatchToContext(const AdbMessage& msg); // 分发消息到上下文
     void setupForwardBridge(uint32_t localId, const std::string& localPort);
-    bool tryGetStreamMessage(uint32_t streamId, AdbMessage &outMsg);
-    bool tryGetMessage(AdbMessage &outMsg);
-    bool matchCommand(const std::vector<uint32_t> &cmdsexpectedCmds, AdbMessage &inputMsg);
 
-private:
+public:
     //接收数据
-    void checkSessionWaiters();   //唤醒
-    std::list<sessionWaiter> sessionWaits;
+    std::unordered_map<uint32_t, std::list<commandWaiter>> streamWaiters;
+    std::deque<AdbMessage> pendingMessages; //未归属队列
+    //WaitCommandAwaitable coWaitCommand(std::vector<uint32_t> expectedCmds, int timeoutMs, uint32_t streamId);
 
-    std::mutex waiterMutex_;
-    std::unordered_map<uint32_t, std::list<commandWaiter>> streamWaits;
-
-
+    asio::awaitable<AdbMessage> coWaitCommand(std::vector<uint32_t> expectedCmds, int timeoutMs, uint32_t streamId);
+    asio::any_io_executor getExecutor() const;
+private:
     std::shared_ptr<asio::ip::tcp::socket> socket_; // 与设备通信的 socket
     std::vector<uint8_t> recvBuffer_; // 接收缓冲，支持粘包处理
-    std::deque<std::vector<uint8_t>> sendQueue_; // 发送队列
-    std::deque<AdbMessage> pendingMessages; //未归属队列
-    std::mutex sendMutex_; // 发送队列线程锁
     std::array<char, 512> tempBuf_; // 临时缓冲区用于读 socket
+    std::mutex sendMutex_; // 发送队列线程锁
+    std::deque<std::vector<uint8_t>> sendQueue_; // 发送队列
     std::map<uint32_t, std::shared_ptr<ForwardBridge>> streamToClient_; // localId 到客户端socket映射
     std::map<uint32_t,std::shared_ptr<AdbStreamContext>>streamContexts; //流对象
     std::set<std::string> openedServices;   //维护流服务的类型
 
+    template<typename T>   //唤醒等待者队列，需要查找的队列
+    void checkWaiters(std::list<T> &Waiters,std::deque<AdbMessage>&deque);
+    std::list<commandWaiter> &findWaiters(uint32_t streamId);
+
 };
+
+//唤醒分两种：事件驱动（有数据时）+ 定时驱动（超时） 后期添加
+template<typename T>
+void DeviceSession::checkWaiters(std::list<T>& Waiters,std::deque<AdbMessage>&deque) {   //单个归属的唤醒者队列
+    qDebug()<<"[checkWaiters]唤醒等待者,当前等待者数量"<<Waiters.size();
+    for (auto it = Waiters.begin(); it != Waiters.end(); ) {
+        if (it->expireTime < std::chrono::steady_clock::now()) {    //超时移除
+            it = Waiters.erase(it);
+            continue;
+        }
+
+        AdbMessage msg;
+        if (findCommandByDeque(it->expectedCmds,deque,msg)) {   //在合适的队列寻找
+            auto cb = std::move(it->notify);  // callback 是 std::function<void(AdbMessage)>
+            it = Waiters.erase(it);        // 先删除，避免迭代器失效
+            cb(msg);
+        } else {
+            ++it;
+        }
+    }
+    qDebug()<<"[checkWaiters]队列现有的包数量:"<<deque.size();
+}
+
+
 
 #endif // DEVICESESSION_H
